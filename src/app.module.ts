3456a3f5ab73e82c959c5b70bf9fb3ee6b52f17b
junho100 <bemodesty306@naver.com>
2022-10-14 17:16:51 +0900

    Feat: RDS(MySQL) 연결 (#ERWT-60)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const returnNodeEnv = () => {
+  if (process.env.NODE_ENV === 'local') {
+    return '.local.env';
+  } else if (process.env.NODE_ENV === 'development') {
+    return '.development.env';
+  } else {
+    return '.production.env';
+  }
+};
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: returnNodeEnv(),
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
